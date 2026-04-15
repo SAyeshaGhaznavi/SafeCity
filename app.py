1bd65906cca51dfb41aa2237ae4ffc70af1bce5b
SAyeshaGhaznavi <syedaayeshaghaznavi@gmail.com>
2026-04-15 16:56:47 +0500

    splash, portal htmls added

--- app.py
+++ app.py
@@ -3,8 +3,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def splash():
+    return render_template('splash.html')
+
+@app.route('/portal')
+def portal():
+    return render_template('portal.html')
+
+@app.route('/report')
+def report():
+    return render_template('report.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
